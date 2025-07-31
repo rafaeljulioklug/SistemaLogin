d64bc3d0dc6d8590b51a3ebc154d09a9fc375bed
rafaelklug <rafa.klug55@gmail.com>
2025-07-31 17:48:15 -0300

    registro-melhorado

--- src/Registro.java
+++ src/Registro.java
@@ -4,17 +4,52 @@ import java.util.Map;
 public class Registro {
     protected Map<Email, Usuario> mapaUsuarios = new HashMap<>();
 
-    //Construtor
-    public Registro(String email, String senha) throws Exception {
-        //Atributos
-        Usuario usuario = new Usuario(email, senha);
 
-        if(mapaUsuarios.containsKey(usuario.getEmail())){
+    public void cadastrarUsuario(String email, String senha) throws Exception {
+        Email chave = new Email(email);
+        if (mapaUsuarios.containsKey(chave)) {
             throw new Exception("Este e-mail já foi cadastrado.");
         }
-        mapaUsuarios.put(usuario.getEmail(), usuario);
+        Usuario novoUsuario = new Usuario(email, senha);
+        mapaUsuarios.put(chave, novoUsuario);
         System.out.println("Usuário cadastrado com sucesso.");
     }
 
-    //Métodos
+
+    public boolean autenticarUsuario(String email, String senha) throws Exception {
+        Email chave = new Email(email);
+        Usuario usuario = mapaUsuarios.get(chave);
+        if (usuario != null && usuario.getSenha().equals(senha)) {
+            System.out.println("Login realizado com sucesso.");
+            return true;
+        } else {
+            System.out.println("E-mail ou senha incorretos.");
+            return false;
+        }
+    }
+
+
+    public boolean removerUsuario(String email) throws Exception {
+        Email chave = new Email(email);
+        if (mapaUsuarios.containsKey(chave)) {
+            mapaUsuarios.remove(chave);
+            System.out.println("Usuário removido.");
+            return true;
+        } else {
+            System.out.println("Usuário não encontrado.");
+            return false;
+        }
+    }
+
+
+    public void listarUsuarios() {
+        if (mapaUsuarios.isEmpty()) {
+            System.out.println("Nenhum usuário cadastrado.");
+            return;
+        }
+
+        for (Map.Entry<Email, Usuario> entry : mapaUsuarios.entrySet()) {
+            System.out.println("Email: " + entry.getKey().getEmail());
+        }
+    }
 }
